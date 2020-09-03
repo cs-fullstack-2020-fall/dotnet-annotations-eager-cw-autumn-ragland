7790300610e97122838814abec9c440b66ef7e2d
autumn-ragland <raglanda998@gmail.com>
2020-09-03 15:02:57 -0500

    complete with validation and comments

--- ActorMovieApplication/Controllers/ActorMovieController.cs
+++ ActorMovieApplication/Controllers/ActorMovieController.cs
@@ -4,6 +4,7 @@ using ActorMovieApplication.Models;
 using System.Linq;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 // handle create and read functionality of movies and actors
 namespace ActorMovieApplication.Controllers
 {
@@ -19,9 +20,23 @@ namespace ActorMovieApplication.Controllers
         [HttpPost]
         public IActionResult AddActor([Bind("actorName")] ActorModel newActor)
         {
-            _context.actors.Add(newActor);
-            _context.SaveChanges();
-            return Content("Successful");
+            // if data passed in body of request is valid
+            if(ModelState.IsValid)
+            {
+                // add actor to actor db set and save changes
+                _context.actors.Add(newActor);
+                _context.SaveChanges();
+                return View("ViewAll", _context);
+            } else 
+            // if data passed in body of request is not valid
+            {
+                // build string to display error from model state query method and pass as view data
+                string displayError = "";
+                List<string> errors = GetErrorListFromModelState(ModelState);
+                errors.ForEach(error => displayError += error);
+                ViewData["displayError"] = displayError;
+                return View("Error");
+            }
         }
         // add movie to database
         [HttpPost]
@@ -29,19 +44,70 @@ namespace ActorMovieApplication.Controllers
         {
             // find actor by id
             ActorModel matchingActor = _context.actors.Include(actor => actor.movies).FirstOrDefault(actor => actor.id == actorID);
-            matchingActor.movies.Add(newMovie);
-            _context.movies.Add(newMovie);
-            _context.SaveChanges();
-            // retrun view one
-            return View("ViewOne", matchingActor);
+            // if matching actor is found
+            if(matchingActor != null)
+            {
+                if(ModelState.IsValid)
+                {
+                matchingActor.movies.Add(newMovie);
+                _context.movies.Add(newMovie);
+                _context.SaveChanges();
+                // retrun view one
+                return View("ViewOne", matchingActor);
+                } else 
+                // if data passed in body of request is not valid
+                {
+                // build string to display error from model state query method and pass as view data
+                    string displayError = "";
+                    List<string> errors = GetErrorListFromModelState(ModelState);
+                    errors.ForEach(error => displayError += error);
+                    ViewData["displayError"] = displayError;
+                    return View("Error");
+                }
+            } else
+            // if matching actor is not found 
+            {
+                // send message as view data
+                ViewData["displayError"] = "No Actor Found";
+                return View("Error");
+            }
+
         }
         // view actor by ID
         [HttpGet]
         public IActionResult ViewOne(int actorID)
         {
             ActorModel matchingActor = _context.actors.Include(actor => actor.movies).FirstOrDefault(actor => actor.id == actorID);
-            // pass found actor to default view
-            return View(matchingActor);
+            // if matching actor is found
+            if(matchingActor != null)
+            {
+                // pass found actor to default view
+                return View(matchingActor);
+            } else
+            // if matching actor is not found
+            {
+                // send message as view data
+                ViewData["displayError"] = "No Actor Found";
+                return View("Error");                
+            }
+            
+        }
+        // view all actors
+        [HttpGet]
+        public IActionResult ViewAll(int actorID)
+        {
+            // pass actors to default view
+            return View(_context);
+        }        
+        // method to capture model state validation errors
+        public static List<string> GetErrorListFromModelState(ModelStateDictionary modelState)
+        {
+            IEnumerable<string> query = from state in modelState.Values
+                        from error in state.Errors
+                        select error.ErrorMessage;
+
+            List<string> errorList = query.ToList();
+            return errorList;
         }
     }
 }
